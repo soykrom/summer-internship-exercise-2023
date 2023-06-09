032385d6f1df3999fc0d0d4257d73cacdc99c574
Soykrom <jmariagundersen@gmail.com>
2023-06-09 18:26:44 +0100

    Solution done. Can be further optimized. Building tests first

--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -18,20 +18,19 @@ public class SnailShellPatternTest {
 
   /**
    * The corresponding implementations to test.
-   *
    * If you want, you can make others :)
    *
    */
   public SnailShellPatternTest() {
-  };
+  }
 
   @Test
-  public void ScreenLockinPatternTestFirst3Length2Test()
+  public void ScreenLockingPatternTestFirst3Length2Test()
       throws InterruptedException, ExecutionException, TimeoutException {
-    int[][] matrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
+    int[][] matrix = { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
     Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
     int[] result = count.get(10, TimeUnit.SECONDS);
-    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
     assertArrayEquals(result, expected);
   }
 }
