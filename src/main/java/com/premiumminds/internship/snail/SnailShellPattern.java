4ce416d6a4a428aad5ab60c42677ebd05b5b92e8
Soykrom <jmariagundersen@gmail.com>
2023-06-09 19:40:36 +0100

    Optimized solution. Added Tests

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,6 +1,5 @@
 package com.premiumminds.internship.snail;
 
-import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -23,48 +22,36 @@ class SnailShellPattern implements ISnailShellPattern {
     int n_i = n;
     int k = (int) Math.ceil((double) n / 2); // Number of matrices
     int[] result = new int[n * n];
-
     int matrix_origin = 0;
-    for (int i = 0; i <= k; i++) {
-      // Top Row
-      int start = matrix_origin;
-      for (int offset = 0; offset < n_i; offset++) {
-        System.out.println("Placing Top: " + matrix[i][offset + i] + " at " + (start + offset));
-        result[start + offset] = matrix[i][offset + i];
+
+    if (matrix.length != 0) {
+
+      for (int i = 0; i <= k; i++) {
+        // Top Row
+        int start = matrix_origin;
+        if (n_i >= 0) System.arraycopy(matrix[i], i, result, start, n_i);
+
+        // Bottom Row
+        start = matrix_origin + 2 * n_i - 2;
+        for (int offset = 0; offset < n_i; offset++) {
+          result[start + (n_i - 1 - offset)] = matrix[n - 1 - i][offset + i];
+        }
+
+        for (int offset = 1; offset < n_i - 1; offset++) {
+          // Left Column
+          start = matrix_origin + 3 * (n_i - 1);
+          result[start + (n_i - 1 - offset)] = matrix[offset + i][i];
+
+          // Right Column
+          start = matrix_origin + n_i - 1;
+          result[start + offset] = matrix[offset + i][n - 1 - i];
+        }
+
+        // go to inner matrix
+        matrix_origin = matrix_origin + (4 * n_i - 4);
+        n_i -= 2;
       }
-
-      // Bottom Row
-      start = matrix_origin + 2 * n_i - 2;
-      for (int offset = 0; offset < n_i; offset++) {
-        System.out.println("Placing Bottom: " + matrix[n - 1 - i][offset + i] + " at " + (start + (n_i - 1 - offset)));
-        result[start + (n_i - 1 - offset)] = matrix[n - 1 - i][offset + i];
-      }
-
-      for (int offset = 1; offset < n_i - 1; offset++) {
-        // Left Column
-        start = matrix_origin + 3 * (n_i - 1);
-        System.out.println("Placing Left: " + matrix[offset + i][i] + " at " + (start + (n_i - 1 - offset)));
-        result[start + (n_i - 1 - offset)] = matrix[offset + i][i];
-
-        // Right Column
-        start = matrix_origin + n_i - 1;
-        System.out.println("Placing Right: " + matrix[offset + i][n - 1 - i] + " at " + (start + offset));
-        result[start + offset] = matrix[offset + i][n - 1 - i];
-      }
-
-      // After going through the top part of every step, do the same for the bottom, left and right sides
-      // No need to do ifs, just add if done correctly => Greatly reduces time
-
-      // go to inner matrix
-      matrix_origin = matrix_origin + (4 * n_i - 4);
-      n_i -= 2;
-
-      System.out.println("i = " + i + " ; matrix_origin = " + matrix_origin + " ; n_i = " + n_i);
-
-      System.out.println("Result after " + (i + 1) + " iteration: " + Arrays.toString(result));
     }
-
-    System.out.println("Result: " + Arrays.toString(result));
 
     return executor.submit(() -> result);
   }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -27,10 +27,58 @@ public class SnailShellPatternTest {
   @Test
   public void ScreenLockingPatternTestFirst3Length2Test()
       throws InterruptedException, ExecutionException, TimeoutException {
-    int[][] matrix = { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
+    int[][] matrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
     Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
     int[] result = count.get(10, TimeUnit.SECONDS);
-    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void EmptyMatrixEdgeCaseTest()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { };
+    assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void SingleEntryMatrixEdgeCaseTest()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { {1} };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1 };
+    assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void FullCoverRegularMatrixTest()
+          throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1,  2,  3,  4,  5 },
+            { 16, 17, 18, 19, 6 },
+            { 15, 24, 25, 20, 7 },
+            { 14, 23, 22, 21, 8 },
+            { 13, 12, 11, 10, 9 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+    assertArrayEquals(result, expected);
+  }
+
+  @Test
+  public void LargeMatrixSolutionTimeTest() {
+    int size = 10000;
+    int[][] matrix = new int[size][size];
+    int el = 1;
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        matrix[i][j] = el++;
+      }
+    }
+    new SnailShellPattern().getSnailShell(matrix);
   }
 }
